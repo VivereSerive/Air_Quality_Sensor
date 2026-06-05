042dcaf39c45cbbd7ae344b9c3ff690fcd109707
Sean Rivera <vivereserive@gmail.com>
2026-06-05 12:42:14 +1200

    Started Working on HTTPS Sketch

---
+++ sketches/ESP32_IoT_HTTPS_Sketch/src/ESP32_IoT_HTTPS_Sketch.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+#include <HTTPClient.h>
+#include <WiFiClientSecure.h>
+#include <WiFi.h>
+
+/*
+  Word
+*/
+
+// Variable Declerations
+
+
+// Function Declerations
+
+
+// Setup
+void setup(){
+
+}
+
+// Main Loop
+void loop(){
+
+}
